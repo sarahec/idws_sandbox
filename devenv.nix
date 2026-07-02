4f4ec1aa44fa11c037b08434c55377690b00e80e
Sarah Clark <seclark@nextquestion.net>
2026-07-02 15:49:02 -0700

    chore: enable Typescript

--- devenv.nix
+++ devenv.nix
@@ -17,6 +17,7 @@
 
   # https://devenv.sh/languages/
   # languages.rust.enable = true;
+  languages.typescript.enable = true;
 
   # https://devenv.sh/processes/
   # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
